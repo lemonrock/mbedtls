53ecb0955455ea297b38c6290c4a00b831cb6132
Raphael Cohn <raphael.cohn@stormmq.com>
2016-08-31 11:37:19 +0100

    Reworked module naming as an experiment. Added from method to CipherSuite

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,13 @@
 // Copyright © 2016 The developers of mbedtls. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/lemonrock/mbedtls/master/COPYRIGHT.
 
 
-mod cipherSuite;
-pub use cipherSuite::*;
-mod sslConfig;
-pub use sslConfig::SslConfig;
-mod sslContext;
-pub use sslContext::SslContext;
+#![feature(associated_consts)]
+#[macro_use] extern crate enum_primitive;
+
+
+#[path="CipherSuite.rs"] mod _CipherSuite;
+pub use _CipherSuite::*;
+#[path="SslConfig.rs"] mod _SslConfig;
+pub use _SslConfig::*;
+#[path="SslContext.rs"] mod _SslContext;
+pub use _SslContext::*;
